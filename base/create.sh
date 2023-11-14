f8b37f9c15266eea341823dd091413299c7f3ee8
Kevin Swinton <kevin.swinton@gmail.com>
2023-11-14 08:52:25 +0000

    testing

---
+++ base/create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# create.sh
+# build and push the base container image
+#
+# Note: DockerHub must be accessible (login via: "docker login")
+
+# construct the base container image name
+dockerHubAct="kevinjswinton"
+dockerHubRpo="helloworld"
+dockerHubImg="web-base"
+imgDatestamp=$(date '+%Y%m%d')
+dockerHubImg=$dockerHubAct/$dockerHubRpo:$dockerHubImg-$imgDatestamp
+
+# build and push the base container image
+echo "[INFO] : Will build and push : $dockerHubImg"
+docker build . --no-cache --tag $dockerHubImg
+docker push                     $dockerHubImg
